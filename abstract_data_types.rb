3b53714e6d90e6775029a651cf43be2bf923cd5c
evekios <imperialraven@gmail.com>
2018-01-19 08:39:02 -0500

    final revision: map complete

--- abstract_data_types.rb
+++ abstract_data_types.rb
@@ -44,18 +44,31 @@ class Map
   end
 
   def assign(key, value)
-    @map << [key, value]
+    kvpair_idx = @map.index {|kvpair| kvpair[0] == key}
+    if kvpair_idx
+      @map[kvpair_idx][1] = value
+    else
+      @map << [key, value]
+    end
   end
 
   def lookup(key)
+    @map.each {|kvpair| kvpair[1] if kvpair[0] == key}
+    nil
   end
 
   def remove(key)
-
+    @map.reject! {|kvpair| pair[0] == key}
+    nil
   end
 
   def show
-    @queue.dup
+    deep_dup(@map)
+  end
+
+  private
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
   end
 
 end
